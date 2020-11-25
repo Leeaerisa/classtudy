cb20dfaf7d46f3cba8291bd68f76ee53d5f8cfac
Leeaerisa <73582933+Leeaerisa@users.noreply.github.com>
2020-11-25 14:39:37 +0900

    ### *프리보드게시판 Mapper*
    #### 1. Freeboard Mapper
    - 작성, 수정, 취소, 목록, Detail, 좋아요, 조회수기능 추가

--- 이애리사/classtudy/src/main/java/com/edu/mapper/FreeboardMapper.java
+++ 이애리사/classtudy/src/main/java/com/edu/mapper/FreeboardMapper.java
@@ -11,20 +11,49 @@ public interface FreeboardMapper {
 
 	
 	//게시글 작성
-	public int writer(FreeboardDTO boardDTO) throws Exception;
+	public int write(FreeboardDTO boardDTO) throws Exception;
+		
+	//게시글 상세보기
+	public FreeboardDTO boardDetail(int boardNo) throws Exception;
+	
+	//게시글 수정
+	public int boardUpdate(FreeboardDTO boarDTO) throws Exception;
+	
+	//게시글 삭제
+	public int boardDelete(int boardNo) throws Exception;
+
+	//게시글 조회수 증가
+	public int addViews(int boardNo) throws Exception;
+	
+	//게시글 좋아요수 증가
+	public int addLikes(int boardNo) throws Exception ;
 	
 	//게시글 목록
 	public List<FreeboardDTO> boardList() throws Exception;
 	
-	//게시글 상세보기
-	public FreeboardDTO boardDetail(int boardNo) throws Exception;
+	//게시글 목록(정해진 말머리)
+	public List<FreeboardDTO> boardList2(String viewCategory, int maxNo) throws Exception;
 	
-	//게시글 조회수 증가
-	public int addViews(int boardNo) throws Exception;
+	//화면에 보여줄 개시글 개수 추출
+	public int getBoardCount() throws Exception;
+	
+	//화면에 보여줄 개시글 개수 추출(정해진 말머리)
+	public int getBoardCount2(String viewCategory) throws Exception;
 
-	//게시글 수정
-	public int update(FreeboardDTO boardDTO) throws Exception;	
+	//마지막 게시글 번호 추출
+	public int getNextNum() throws Exception;
+
+	//마지막 게시글 번호 추출 (정해진 말머리만)
+	public int getNextNum2(String viewCategory) throws Exception;
+
+	//게시글 검색
+	public List<FreeboardDTO> search(String keyword) throws Exception;
+
+	//게시글 검색(정해진 말머리)
+	public List<FreeboardDTO> search2(String keyword, String viewCategory) throws Exception;
 	
-	//게시글 삭제
+	
+	
+
 
 }
